a969fe76e059142491b9407472d562ff84a7f233
Gian Riske <gian.riske@gmail.com>
2019-10-22 20:05:12 -0300

    Configurado nova conexão com banco de dados, Alterações na tela de Cadastro de usuarios.

--- src/View/CadastroUsuarioController.java
+++ src/View/CadastroUsuarioController.java
@@ -5,9 +5,17 @@
  */
 package View;
 
+import Logica.TelaLoginMain;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import model.bean.Usuario;
+import model.dao.UsuarioDAO;
 
 /**
  * FXML Controller class
@@ -18,10 +26,51 @@ public class CadastroUsuarioController implements Initializable {
 
     /**
      * Initializes the controller class.
+     *
+     * @param url
      */
+    @FXML
+    ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btSalvar;
+
+    @FXML
+    private Button btCancelar;
+
+    @FXML
+    private Button btNovo;
+
+    @FXML
+    private TextField pfSenha;
+
+    @FXML
+    private TextField tfLogin;
+
+    @FXML
+    private TextField tfNome;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+        btCancelar.setOnMouseClicked((MouseEvent e) -> {
+            fecha();
+        });
+    }
+
+    public void fecha() {
+        TelaLoginMain.getStage().close();
+    }
+
+    @FXML
+
+    public void salvar(MouseEvent event) {
+        Usuario u = new Usuario(0, tfNome.getText(), tfLogin.getText(), pfSenha.getText());
+        UsuarioDAO dao = new UsuarioDAO();
+        dao.create(u);
+
+    }
 }
--- src/model/dao/UsuarioDAO.java
+++ src/model/dao/UsuarioDAO.java
@@ -5,10 +5,33 @@
  */
 package model.dao;
 
+import Conexão.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import model.bean.Usuario;
+
 /**
  *
  * @author Elison Christoph
  */
 public class UsuarioDAO {
-    
+
+    public void create(Usuario u) {
+        Connection con = Conexao.getConnection();
+        PreparedStatement stmt = null;
+        try {
+            stmt = con.prepareStatement("INSERT INTO usuario (nome, login, senha)VALUES(?, ?, ?)");
+            stmt.setString(1, u.getNome());
+            stmt.setString(2, u.getLogin());
+            stmt.setString(3, u.getSenha());
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Salvo com sucesso!");
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            Conexao.closeConnection(con, stmt);
+        }
+    }
 }
