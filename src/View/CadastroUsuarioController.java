b93509abdf2c4686315c86254107371d89c3672e
Gian Riske <gian.riske@gmail.com>
2019-10-22 22:26:00 -0300

    Alterações na tela de usuarios.

--- src/View/CadastroUsuarioController.java
+++ src/View/CadastroUsuarioController.java
@@ -7,13 +7,21 @@ package View;
 
 import Logica.TelaLoginMain;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Font;
+import javax.swing.DefaultListModel;
 import model.bean.Usuario;
 import model.dao.UsuarioDAO;
 
@@ -29,33 +37,36 @@ public class CadastroUsuarioController implements Initializable {
      *
      * @param url
      */
-    @FXML
     ResourceBundle resources;
-
-    @FXML
-    private URL location;
-
-    @FXML
-    private Button btSalvar;
-
-    @FXML
-    private Button btCancelar;
-
-    @FXML
-    private Button btNovo;
-
-    @FXML
-    private TextField pfSenha;
-
-    @FXML
-    private TextField tfLogin;
-
+    private ObservableList lista;
+    List<Usuario> usuarios;
+    Usuario usuario;
+    UsuarioDAO dao;
     @FXML
     private TextField tfNome;
+    @FXML
+    private Font x2;
+    @FXML
+    private Button btNovo;
+    @FXML
+    private Font x1;
+    @FXML
+    private Button btSalvar;
+    @FXML
+    private Button btCancelar;
+    @FXML
+    private TextField tfLogin;
+    @FXML
+    private ListView<?> lvUsuarios;
+    @FXML
+    private PasswordField pfSenha;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        lista = FXCollections.observableArrayList();
+        usuarios = new ArrayList<>();
+        dao = new UsuarioDAO();
+        listar();
         btCancelar.setOnMouseClicked((MouseEvent e) -> {
             fecha();
         });
@@ -69,8 +80,28 @@ public class CadastroUsuarioController implements Initializable {
 
     public void salvar(MouseEvent event) {
         Usuario u = new Usuario(0, tfNome.getText(), tfLogin.getText(), pfSenha.getText());
-        UsuarioDAO dao = new UsuarioDAO();
         dao.create(u);
+
+    }
+
+    public void listar() {
+        usuarios.clear();
+        lista.clear();
+        usuarios = dao.read();
+        for (Usuario u : usuarios) {
+            lista.add(u.getNome());
+        }
+        lvUsuarios.setItems(lista);
+    }
+
+    @FXML
+    public void selecionaUsuario() {
+        int index = lvUsuarios.getSelectionModel().getSelectedIndex();
+        if (index >= 0) {
+            tfNome.setText(usuarios.get(index).getNome());
+            tfLogin.setText(usuarios.get(index).getLogin());
+            pfSenha.setText(usuarios.get(index).getSenha());
+        }
 
     }
 }
--- src/model/dao/UsuarioDAO.java
+++ src/model/dao/UsuarioDAO.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.dao;
 
 import Conexão.Conexao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import model.bean.Usuario;
 
@@ -33,5 +33,27 @@ public class UsuarioDAO {
         } finally {
             Conexao.closeConnection(con, stmt);
         }
+    }
+
+    public List<Usuario> read() {
+        Connection con = Conexao.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<Usuario> usuarios = new ArrayList<>();
+        
+        try {
+            stmt = con.prepareStatement("SELECT * FROM usuario");
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Usuario usuario = new Usuario(rs.getInt("id"), rs.getString("nome"), rs.getString("login"), rs.getString("senha"));
+                usuarios.add(usuario);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            Conexao.closeConnection(con, stmt, rs);
+        }
+        return usuarios;
     }
 }
